ea2ec75c1bd5574a99b5b0330ac924bcb5cb88f7
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2023-12-02 11:47:21 +0530

    Update interview.js

--- interview.js
+++ interview.js
@@ -1,11 +1,11 @@
-// 0 1 2 3 4 => let (global scoped)
+// 0 1 2 3 4 => let (block scoped)
 for (let i = 0; i < 5; i++) {
     setTimeout(() => {
         // console.log(i);
     }, 2000);
 }
 
-// 5 5 5 5 5 => var (points to same memory location | function scoped)
+// 5 5 5 5 5 => var (points to same memory location | gobal scoped)
 for (var i = 0; i < 5; i++) {
     setTimeout(() => {
         // console.log(i);
