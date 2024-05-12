90eba39f8dc4c427f9abe0a3f5a92c37a98f9441
Allan Joston Fernandes <allanfernandes459@gmail.com>
2024-05-12 23:35:29 +0530

    observer pattern

---
+++ js-design-patterns/observer/index.js
@@ -0,0 +1,36 @@
+class Observer {
+    constructor(){
+        this.observers = [];
+    }
+    
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    removeObserver(observer) {
+        this.observers = this.observers.filter((ob) => observer !== ob);
+    }
+
+    notifyObserver(data) {
+        this.observers.map((observer) => observer(data));
+    }
+}
+
+const observer = new Observer();
+
+function observer1(data) {
+    console.log(`observer1: ${data}`)
+}
+
+function observer2(data) {
+    console.log(`observer2: ${data}`)
+}
+
+observer.addObserver(observer1);
+observer.addObserver(observer2);
+
+observer.notifyObserver("foo");
+
+observer.removeObserver(observer1);
+
+observer.notifyObserver("observer1 removed");
