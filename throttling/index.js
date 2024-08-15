326d6c1c801ef8923bd9bbdbc63d33a58fde8006
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2024-08-15 16:18:21 +0530

    Update index.js

--- throttling/index.js
+++ throttling/index.js
@@ -3,12 +3,13 @@ const getData = () => {
     console.log("Throttling...", counter++);
 }
 
+// solution-1
 const throttle = (cb, limit) => {
     let flag=true; // to restrict the function from executing for delay of 'limit'
     return function(...args){
         let context=this;
         if(flag){
-            cb.aply(context, args);
+            cb.apply(context, args);
             flag=false;
             setTimeout(() => {
                 flag=true;
@@ -18,3 +19,22 @@ const throttle = (cb, limit) => {
 }
 
 const betterFunction = throttle(getData, 300);
+
+// solution - 2
+function throttle(func, wait) {
+  let timer = null;
+  let lastArgs = null;
+  
+  return function(...args) {
+    if(timer) {
+      lastArgs = args;
+    } else {
+      func.apply(this, args);
+      timer = setTimeout(()=>{
+        if(lastArgs) {
+          func.apply(this, lastArgs);
+        }
+      }, wait);
+    }
+  }
+}
