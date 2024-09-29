e114c51ccadece74247792766be7e47a3de1aafa
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2024-09-29 18:42:21 +0530

    Update index.js

--- js-design-patterns/singleton.js/index.js
+++ js-design-patterns/singleton.js/index.js
@@ -1,6 +1,15 @@
 /*
 The Singleton Pattern limits the number of instances of a particular object to just one. This single instance is called the singleton.
 Ex: database connection pool
+
+Advantages:
+- It ensures that there is only one instance of a class, which can be useful for coordinating actions across a system. 
+- It provides a global point of access to that instance, which can be useful for sharing resources or communicating between different parts of an application. 
+- It can help to reduce the memory footprint of your application, as you only need to create one instance of a class.
+
+Disadvantages:
+- Testing becomes more difficult.
+- Use of a Singleton can lead to tight coupling between classes, which can make your code less flexible and harder to maintain.
 */
 
 function Counter() {
