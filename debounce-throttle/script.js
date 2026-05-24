e9db15c14224eafc346366e9e7b2367f1e0c9458
Allan Joston Fernandes <54631653+Allan2000-Git@users.noreply.github.com>
2026-05-24 17:23:08 +0530

    Comment out throttle function in script.js
    Comment out the throttle function in script.js.

--- debounce-throttle/script.js
+++ debounce-throttle/script.js
@@ -37,6 +37,28 @@ function throttle(func, delay = 1000) {
   }
 }
 
+// function throttle(fn, delay) {
+//   let lastRun = 0;
+//   let timer;
+
+//   return function (...args) {
+//     const now = Date.now();
+//     const remaining = delay - (now - lastRun);
+
+//     if (remaining <= 0) {
+//       clearTimeout(timer);
+//       lastRun = now;
+//       fn.apply(this, args);
+//     } else {
+//       clearTimeout(timer);
+//       timer = setTimeout(() => {
+//         lastRun = Date.now();
+//         fn.apply(this, args);
+//       }, remaining);
+//     }
+//   };
+// }
+
 document.addEventListener("mousemove", e => {
     incrementCount(defaultText)
     updateDebounceText()
